820a9a3f4cf889adca912b23400a1b12d75fc1b3
PakizaMutallimova <pakizemutellimova97@mail.ru>
2024-06-13 22:47:56 +0400

    some code wrote...

--- chat-project/src/components/Form/index.jsx
+++ chat-project/src/components/Form/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// import Firebase from '../../firebase'
 
 const Form = () => {
   const [action, setaction] = useState('Login')
--- chat-project/src/firebase.js
+++ chat-project/src/firebase.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCf3gpw8ItToKGzRVWPDTm-4o3zGIomWQU",
   authDomain: "chat-data-9e447.firebaseapp.com",
@@ -14,5 +9,5 @@ const firebaseConfig = {
   appId: "1:850236885081:web:b3e25842e54128d0a40948"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(app);
